4a9c05af895f3605a29616432f9823188a8136c6
sayakdutta <35802924+sayakdutta@users.noreply.github.com>
2019-11-04 21:03:41 +0530

    buzzer code

--- Project/buzz.py
+++ Project/buzz.py
@@ -1,10 +1,59 @@
-from gpiozero import Buzzer
+#!/usr/bin/python
+
+import os
 from time import sleep
-buzzer = Buzzer(17)
-while True:
-    buzzer.on()
-    sleep(1)
-    buzzer.off()
-    sleep(1)
-while True:
-    buzzer.beep()
+import RPi.GPIO as GPIO
+
+BUZZ_PIN = 33
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(BUZZ_PIN,GPIO.OUT)
+
+loop_count = 0
+
+# define a function called morsecode
+def morsecode ():
+    #Dot Dot Dot
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.1)
+
+    #Dash Dash Dash
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.2)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.2)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.2)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.2)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.2)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.2)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.2)
+
+    #Dot Dot Dot
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.HIGH)
+    sleep(.1)
+    GPIO.output(BUZZ_PIN,GPIO.LOW)
+    sleep(.7)
+
+#os.system('clear')
+print("Morse Code")
+morsecode()
